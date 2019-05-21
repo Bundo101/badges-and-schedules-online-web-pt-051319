05208125da2ac5e4741542c8b2e3f81919539c4c
James Britton <brittonjp@gmail.com>
2019-05-21 13:01:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array_of_names)
-  
+  new_array = []
+  array_of_names.each do |element|
+    new_array << "Hello, my name is #{element}."
+  end
+  new_array
 end
